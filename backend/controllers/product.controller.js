3a25523ad76aa9135dcf5f40ebf95e7c0227912f
Ebram-Y-assaad <ebrambotros84@gmail.com>
2024-11-07 16:29:22 +0200

    app-list

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,70 @@
+import Product from "../models/product.model.js"
+
+
+
+
+export const getProducts = async(req,res) => {
+    try{
+      const products = await Product.find({});
+      res.status(200).json({success:true, datat:products});
+    }catch(error){
+      console.log("error in fetching products:", error.massage)
+      res.status(500).json({success:false, massage:"Server Error"})
+    }
+  }
+
+
+  export const createProduct = async(req, res) => {
+    const product = req.body; //user will send this data
+  
+    if(!product.name || !product.price || !product.image){
+      return res.status(400).json({success:false, massage:"Please provide all fields"});
+    }
+  
+    const newProduct = new Product(product);
+    try{
+      await newProduct.save();
+      res.status(201).json({success:true, data:newProduct});
+    }
+    catch(error){
+      console.error("Error in Create product",error.massage);
+      res.status(500).json({success:false, massage:"Server Error"})
+    }
+  };
+
+
+  export const updateProduct = async(req, res)=>{
+  
+    const {id} =req.params;
+    const product = req.body;
+  
+    if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({success:false,massage:"Invalid Product id"})
+    }
+  
+    try{
+      const updatedProduct = await Product.findByIdAndUpdate(id, product,{new:true});
+      res.status(200).json({success:true, data:updatedProduct})
+    }catch(error){
+      res.status(500).json({success:false,massage:"Server error"})
+    }
+  };
+
+
+
+
+  export const deleteProduct = async(req,res) => {
+    const {id}= req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false,massage:"Invalid Product id"})
+      }
+
+    try{
+      await Product.findByIdAndDelete(id);
+      res.status(200).json({success:true, massage:"Product deleted"})
+    } catch (error){
+      console.log("error in deleting product :", error.massage)
+      res.status(404).json({success:false, massage: "Product not found"})
+    }
+  }
